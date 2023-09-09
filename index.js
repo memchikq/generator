26549b4a97c14fd8b7240bcdce217a0a3b62030a
Derks <derks_derks@mail.ru>
2023-09-09 17:56:23 +0600

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,"public")));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
